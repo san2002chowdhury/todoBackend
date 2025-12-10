851e03d94e663d8c82eef7d3fc07b5c36848a258
Santanu <sunilkrchowdhury66@gmail.com>
2025-12-10 09:33:26 +0530

    server fix

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import { dbConnect } from "./src/config/dbConnect.js";
 import dotenv from 'dotenv'
 import userRoute from "./src/routes/userRoute.js";
 import todoRoute from "./src/routes/todoRoute.js";
+import multerRoute from "./src/routes/multerRoute.js";
+
 dotenv.config()
 
 const app = express();
@@ -11,8 +13,11 @@ const port = process.env.PORT || 8001;
 dbConnect();
 
 app.use(express.json());
+app.use("/upload", express.static("upload"))
+
 app.use("/user", userRoute)
 app.use("/todo", todoRoute)
+app.use("/picture", multerRoute)
 
 app.listen(port, () => {
     console.log(`<================Server Started At-${port}=================>`);
